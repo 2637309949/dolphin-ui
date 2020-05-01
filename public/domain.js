99ed1f1a256ad625b0a9d958a97a166d6f6a266e
double <2637309949@qq.com>
2020-05-01 11:33:07 +0800

    fix: Domain

--- public/domain.js
+++ public/domain.js
@@ -1,11 +1,11 @@
 window.Domain = {
-    "name": "127.0.0.1",
-    "full_name": "127.0.0.1",
+    "name": "localhost",
+    "full_name": "localhost",
     "contact_name": null,
     "contact_email": null,
     "contact_mobile": null,
-    "login_url": "127.0.0.1",
-    "api_url": "http://127.0.0.1:8082",
+    "login_url": "localhost",
+    "api_url": "http://localhost:8082",
     "static_url": null,
     "theme": "default",
     "auth_mode": 1
