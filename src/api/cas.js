597989e532c48c662b934fed484c5d7a1b420caa
double <2637309949@qq.com>
2020-05-13 19:55:57 +0800

    style: _

--- src/api/cas.js
+++ src/api/cas.js
@@ -1,4 +1,3 @@
-
 export function url(redirect, state) {
   window.open(`${window.Domain.api_url}/api/sys/cas/url?redirect_uri=${encodeURIComponent(`${window.location.origin}/#${redirect}`)}&state=${state}`, true)
 }
